95693c8a51fb4f8408f616bf4c08c81ae1f92ba1
denakof <denakofahi_21@yahoo.com>
2021-06-28 15:16:12 +0300

    day1

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                Dena Kofahi &copy; 2021
+            </div>
+        )
+    }
+}
+
+export default Footer
