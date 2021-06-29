ccca82b88b1ec176a521eb4e0ad58f8542aa78bf
denakof <denakofahi_21@yahoo.com>
2021-06-29 14:28:30 +0300

    lab1-class1-solution

---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hornedbeasts from './Hornedbeasts'
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <Hornedbeasts
+                />
+            </div>
+        )
+    }
+}
+export default Main;
