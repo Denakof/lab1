20aa1ae07325a6910a8d867a3aa6d853a226c475
Denakof <82310640+Denakof@users.noreply.github.com>
2021-07-10 22:37:07 +0300

    Update SelectedBeast.js

--- src/component/SelectedBeast.js
+++ src/component/SelectedBeast.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
 class SelectedBeast extends React.Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Modal show={this.props.show} onHide={this.props.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.title}</Modal.Title>
+            
+          </Modal.Header>
+          <Modal.Body>
+            {this.props.par} <br></br>
+            <br></br>
+            Horns number is = {this.props.num}
+          </Modal.Body>
+          <img src={this.props.imgUrl} />
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 }
 
-export default SelectedBeast
+export default SelectedBeast;
