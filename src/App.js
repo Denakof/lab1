92f95184146ef1bc23f7dbcf998d928680bcd3e6
Denakof <82310640+Denakof@users.noreply.github.com>
2021-07-10 22:37:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from 'react'
-import Footer from './component/Footer'
-import Header from './component/Header'
-import Main from './component/Main'
-import SelectedBeast from './component/SelectedBeast'
-
-
+import React from "react";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import SelectedBeast from "./component/SelectedBeast";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       Selectedpic: null,
-
-    }
+      show: false,
+    };
   }
 
-  Selectcard = (param) => {
-    this.setState(
-      { Selectedpic: param }
-    )
+  Selectcard = (title, par, num, imgUrl) => {
+    this.setState({
+      show: true,
+    });
+    this.setState({
+      Selectedpic: (
+        <SelectedBeast
+          handleClose={this.handleClose}
+          title={title}
+          par={par}
+          num={num}
+          imgUrl={imgUrl}
+          show={this.state.show}
+        />
+      ),
+    });
+  };
 
-  }
-
+  handleClose = () => {
+    console.log("Close");
+    this.setState({
+      show: false,
+      Selectedpic: null,
+    });
+  };
 
   render() {
     return (
@@ -32,7 +48,7 @@ class App extends React.Component {
         <Main selectfun={this.Selectcard} />
         <Footer />
       </>
-    )
+    );
   }
 }
 
