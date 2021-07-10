1b23aa175a9cc94b802ca937b56c311fbab01e8a
Denakof <82310640+Denakof@users.noreply.github.com>
2021-07-10 22:36:39 +0300

    Update Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,22 +1,69 @@
-import React from 'react'
-import Hornedbeasts from './Hornedbeasts'
-import beasts from './Data.json'
+import React from "react";
+import Hornedbeasts from "./Hornedbeasts";
+import beasts from "./Data.json";
+import { Form, Button } from "react-bootstrap";
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      horns: true,
+      filteredBeasts: beasts,
+    };
+  }
 
+  SubmitForm = (event) => {
+    event.preventDefault();
+    console.log(event.target.Options.value);
+    this.setState({
+      filteredBeasts: beasts.filter((element) => {
+        if (event.target.Options.value == "0") {
+          return true;
+        } else {
+          if (element.horns == event.target.Options.value) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }),
+    });
+  };
 
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.SubmitForm}>
+          <Form.Group aria-label="Default select example">
+            <Form.Label>Choose required Horns number</Form.Label>
 
-    render() {
+            <Form.Control as="Select" name="Options">
+              <option value="0">All of them</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
+            </Form.Control>
+          </Form.Group>
 
-        return (
-            <div>
-                {beasts.map((element, index) => {
-                    return <Hornedbeasts selectfun={this.props.selectfun} key={index} title={element.title} Imgurl={element.image_url} par={element.description} num={element.horns} />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
-                })
-                }
-
-            </div>
-        )
-    }
+        {this.state.filteredBeasts.map((element, index) => {
+          return (
+            <Hornedbeasts
+              selectfun={this.props.selectfun}
+              key={index}
+              title={element.title}
+              Imgurl={element.image_url}
+              par={element.description}
+              num={element.horns}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default Main;
